9a1ff3f179e0de105c0b713126d7efcce81e71b7
AJ <aj.monty@gmail.com>
2021-02-09 10:50:37 +0000

    tidying up and single line each block

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 gem 'rspec'
+gem 'rubocop'
 gem 'simplecov', require: false, group: :test
--- lib/bank.rb
+++ lib/bank.rb
@@ -1,41 +1,39 @@
+# frozen_string_literal: true
+
 class Bank
+  attr_reader :balance, :transactions
 
-    attr_reader :balance, :transactions
+  def initialize
+    @balance = 0
+    @transactions = []
+  end
 
-    def initialize()
-        @balance = 0
-        @transactions = []
-    end
+  def deposit(date:, credit_amount:)
+    # adds to balance
+    @balance += credit_amount
+    # then calls add_to_transactions to save state
+    add_to_transactions(date: date, credit_amount: credit_amount)
+  end
 
-    def deposit(date:, credit_amount:)
-        # adds to balance
-        @balance += credit_amount
-        # then calls add_to_transactions to save state
-        add_to_transactions(date: date, credit_amount: credit_amount)
-    end
+  def withdraw(date:, debit_amount:)
+    # removes from balance
+    @balance -= debit_amount
+    # then calls add_to_transactions to save state
+    add_to_transactions(date: date, debit_amount: debit_amount)
+  end
 
-    def withdraw(date:, debit_amount:)
-        # removes from balance
-        @balance -= debit_amount
-        # then calls add_to_transactions to save state
-        add_to_transactions(date: date, debit_amount: debit_amount)
-    end
+  def print_transactions
+    # defines the columns in the method rather than having the first index of the array set to:
+    puts '  date  ||  credit  ||  debit  ||  balance'
+    # puts all transactions in reverse order
+    @transactions.reverse.each { |transaction| puts transaction }
+  end
 
-    def print_transactions
-        # defines the columns in the method rather than having the first index of the array set to:
-        puts "  date  ||  credit  ||  debit  ||  balance"
-        # puts all transactions in reverse order
-        @transactions.reverse.each do |transaction|
-            puts transaction
-        end
-    end
+  private
 
-    private
-
-    # added spacing for command line formatting (works best with 4 digit numbers)
-    # this method adds the transaction with date to transactions array
-    def add_to_transactions(date:, credit_amount: '    ', debit_amount: '    ')
-        @transactions << "#{date}||   #{credit_amount}   ||   #{debit_amount}  ||  #{@balance}"
-    end
-
+  # added spacing for command line formatting (works best with 4 digit numbers)
+  # this method adds the transaction with date to transactions array
+  def add_to_transactions(date:, credit_amount: '    ', debit_amount: '    ')
+    @transactions << "#{date}||   #{credit_amount}   ||   #{debit_amount}  ||  #{@balance}"
+  end
 end
